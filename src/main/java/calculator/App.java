5f2def7a2d824fd99666b1850da3b97fa88f3f75
Robbie <robbie331@>
2024-04-30 22:12:23 +0900

    Level1 -2

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class App {
 
+
     public static void main(String[] args) {
                 Scanner sc = new Scanner(System.in);
 
@@ -12,6 +13,10 @@ public class App {
                 System.out.print("두 번째 숫자를 입력하세요: ");
                 int num2 = sc.nextInt();
 
-                System.out.println("num1, num2 = " + num1 + ", " + num2);
+                System.out.print("사칙연산 기호를 입력하세요: "); // +, -, *, /
+                char operator = sc.next().charAt(0);
+                /* charAt(idx) : charAt 메서드는 매개변수로 char 타입으로 반환하고자 하는 문자열의 위치를 받는다*/
+
+                System.out.println("operator = " + operator);
     }
 }

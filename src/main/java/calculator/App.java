e31d78659ff1748748c19c6237eab8dd2b56cd1f
Robbie <robbie331@>
2024-05-01 11:20:00 +0900

    깃허브연동

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -1,5 +1,8 @@
 package calculator;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
@@ -7,6 +10,17 @@ public class App {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        linkedList.add(1);
+        linkedList.add(2);
+        linkedList.add(3);
+        linkedList.add(4);
+        linkedList.add(5);
+        linkedList.add(6);
+        linkedList.add(7);
+        linkedList.add(8);
+        linkedList.add(9);
+        linkedList.add(10);
 
         System.out.print("첫 번째 숫자를 입력하세요: ");
         int a = sc.nextInt();
@@ -14,6 +28,7 @@ public class App {
         int b = sc.nextInt();
         System.out.print("사칙연산 기호를 입력하세요: "); // +, -, *, /
         String c = sc.next();
+
 
 
         int result = 0;
@@ -44,6 +59,16 @@ public class App {
 
 
             }
+            System.out.println(linkedList.get(1));
+            System.out.println(linkedList.get(2));
+            System.out.println(linkedList.get(3));
+            System.out.println(linkedList.get(4));
+            System.out.println(linkedList.get(5));
+            System.out.println(linkedList.get(6));
+            System.out.println(linkedList.get(7));
+            System.out.println(linkedList.get(8));
+            System.out.println(linkedList.get(9));
+            System.out.println(linkedList.get(10));
             System.out.print("더 계산하시겠습니까? (exit 입력 시 종료) ");
             Scanner scan = null;
             String quite = scan.nextLine();

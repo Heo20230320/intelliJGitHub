4cae2be6ed7404b4b08353baaedff48f56090674
Robbie <robbie331@>
2024-05-02 17:40:25 +0900

    Level 2-6

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -8,8 +8,7 @@ public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        Calculator calculator = new Calculator();
-
+        Calculator calculator = new Calculator(new ArrayList<>());
 
 
     do {
--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -6,10 +6,11 @@ import java.util.List;
 
 public class Calculator {
 
-    private List<Integer> results = new ArrayList<>();
+    private List<Integer> results;
 
-
-
+    public Calculator(List<Integer> results) {
+        this.results = results;
+    }
 
     public int calculate(int num1, int num2, char operator) {
         int result = 0;

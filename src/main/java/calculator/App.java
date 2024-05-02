85d84cd4d922cb4dedea3929703dadd18115d878
Robbie <robbie331@>
2024-05-02 16:42:22 +0900

    Level 1-8

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -54,7 +54,12 @@ public class App {
             results.remove(0);
         }
 
-        System.out.println("결과 = " + results);
+        System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
+        if(Objects.equals(sc.next(), "inquiry")) {
+            for (Integer res : results) {
+                System.out.println("연산결과 : " + res);
+            }
+        }
 
 
         System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");

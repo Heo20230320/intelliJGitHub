57a165b5104c83936687f18f7f3841f706a70365
Robbie <robbie331@>
2024-05-02 17:09:51 +0900

    Level 2-2

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -8,7 +8,7 @@ public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        List<Integer> results = new ArrayList<>();
+        Calculator calculator = new Calculator();
 
 
 
@@ -21,42 +21,17 @@ public class App {
         System.out.println("사칙연산 기호를 입력하세요: "); // +, -, *, /
         char operator = sc.next().charAt(0);
 
-        int result = 0;
-
-        switch (operator) {
-            case '+':
-                result = num1 + num2;
-                break;
-            case '-':
-                result = num1 - num2;
-                break;
-            case '*':
-                result = num1 * num2;
-                break;
-            case '/':
-                if (num2 != 0) {
-                    result = num1 / num2;
-                } else {
-                    System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
-                    return;
-                }
-                break;
-            default:
-                System.out.println("올바른 선택이 아닙니다.");
-                return;
-
-        }
-        System.out.println("결과 : " + result);
-        results.add(result);
+        int result = calculator.calculate(num1, num2, operator);
+        calculator.results.add(result);
 
         System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
         if(Objects.equals(sc.next(), "remove")) {
-            results.remove(0);
+            calculator.results.remove(0);
         }
 
         System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
         if(Objects.equals(sc.next(), "inquiry")) {
-            for (Integer res : results) {
+            for (Integer res :  calculator.results) {
                 System.out.println("연산결과 : " + res);
             }
         }
